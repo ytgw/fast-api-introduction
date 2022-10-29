5d3b666a75c2bc7df110ed7af2dc57eda3f0490e
ytgw <tagawa0920@gmail.com>
2022-10-29 16:45:39 +0900

    リクエスト含めスキーマを定義。

--- api/routers/task.py
+++ api/routers/task.py
@@ -10,16 +10,20 @@ async def list_tasks() -> list[task_schema.Task]:
     return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 
-@router.post("/tasks")
-async def create_task() -> None:
-    pass
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(
+    task_body: task_schema.TaskCreate,
+) -> task_schema.TaskCreateResponse:
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
 
-@router.put("/tasks/{task_id}")
-async def update_task() -> None:
-    pass
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(
+    task_id: int, task_body: task_schema.TaskCreate
+) -> task_schema.TaskCreateResponse:
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
 
 
-@router.delete("/tasks/{task_id}")
-async def delete_task() -> None:
-    pass
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_task(task_id: int) -> None:
+    return None
--- api/schemas/task.py
+++ api/schemas/task.py
@@ -3,7 +3,24 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
 class Task(BaseModel):
     id: int
-    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
     done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
