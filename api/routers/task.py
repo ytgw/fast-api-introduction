610059302c848e94399837344f530c5cc244f6df
ytgw <tagawa0920@gmail.com>
2022-10-29 16:25:14 +0900

    レスポンススキーマを追加。

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,23 +1,25 @@
 from fastapi import APIRouter
 
+import api.schemas.task as task_schema
+
 router = APIRouter()
 
 
-@router.get("/tasks")
-async def list_tasks():
-    pass
+@router.get("/tasks", response_model=list[task_schema.Task])
+async def list_tasks() -> list[task_schema.Task]:
+    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 
 @router.post("/tasks")
-async def create_task():
+async def create_task() -> None:
     pass
 
 
 @router.put("/tasks/{task_id}")
-async def update_task():
+async def update_task() -> None:
     pass
 
 
 @router.delete("/tasks/{task_id}")
-async def delete_task():
+async def delete_task() -> None:
     pass
