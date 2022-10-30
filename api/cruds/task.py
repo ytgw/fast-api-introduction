09c49a53f835cad0c921fb278d81ffbc49a57037
ytgw <tagawa0920@gmail.com>
2022-10-30 15:26:21 +0900

    タスク削除API実行時にDBから削除するように変更。

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -52,3 +52,8 @@ async def update_task(
     await db.commit()
     await db.refresh(original)
     return original
+
+
+async def delete_task(db: AsyncSession, original: task_model.Task) -> None:
+    await db.delete(original)
+    await db.commit()
--- api/routers/task.py
+++ api/routers/task.py
@@ -31,5 +31,9 @@ async def update_task(
 
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int) -> None:
-    return None
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)) -> None:
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.delete_task(db, original=task)
